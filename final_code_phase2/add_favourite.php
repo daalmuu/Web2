a4cbe08d5e6ef2ab1a7d92b0c9fde12950d03b0c
Ghala Almshegheh <G.Almshegheh@outlook.com>
2026-04-20 23:13:36 +0300

    Update add_favourite.php

--- final_code_phase2/add_favourite.php
+++ final_code_phase2/add_favourite.php
@@ -1,6 +1,6 @@
 <?php
-include('session.php');
-include('DB.php');
+require_once("session.php");
+require_once("DB.php");
 
 if (empty($_GET['recipe_id'])) {
     header("Location: User-dashboard.php");
