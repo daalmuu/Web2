c2c44eb11af5e5c9a762acb1fe0aad9d6fdb0e37
Ghala Almshegheh <G.Almshegheh@outlook.com>
2026-04-20 23:14:38 +0300

    Update admin_session.php

--- final_code_phase2/admin_session.php
+++ final_code_phase2/admin_session.php
@@ -1,7 +1,6 @@
 <?php
-include('session.php');
-include('DB.php');
-
+require_once("session.php");
+require_once("DB.php");
 if (!isset($_SESSION['usertype']) || $_SESSION['usertype'] !== 'admin') {
     header("Location: login.php?error=Access+denied");
     exit();
