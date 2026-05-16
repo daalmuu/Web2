5396efab10656a5ce0bec0acec113146cfb674c9
daalmuu <145475913+daalmuu@users.noreply.github.com>
2026-05-16 11:04:23 +0300

    Update DB.php

--- final_code_phase3/DB.php
+++ final_code_phase3/DB.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "root", "bellacucina_db", 8889);
+$conn = new mysqli(
+    "sql200.infinityfree.com",
+    "if0_41918251",
+    "dalalmohammed",
+    "if0_41918251_bellacucina_db",
+    3306
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
